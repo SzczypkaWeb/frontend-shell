4b7cee5e299188566b2da04b3c8f8579ff534f27
szczyppka <p.szczypka.dev@gmail.com>
2026-07-25 23:33:09 +0200

    chore: add lefthook pre-commit hooks

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,25 @@
-import { useUsers } from "./hooks/useUsers"
+import { useUsers } from './hooks/useUsers';
 import { useUIStore } from './store/uiStore';
 
 export default function App() {
-  const {data, isLoading, error} = useUsers();
-  const { theme, toggleTheme } = useUIStore();
+  const { data, isLoading, error } = useUsers();
+  const {
+    theme,
 
-  if(isLoading) return <p>Ładowanie...</p>
-  if(error) return <p>Błąd: {(error as Error).message}</p>
+    toggleTheme,
+  } = useUIStore();
+
+  if (isLoading) return <p>Ładowanie...</p>;
+  if (error) return <p>Błąd: {(error as Error).message}</p>;
   return (
     <div>
       <h1>shell działa</h1>
       <button onClick={() => toggleTheme()}>{theme}</button>
-      <ul>{data?.map((u) => <li key={u.id}>{u.email}</li>)}</ul>
+      <ul>
+        {data?.map((u) => (
+          <li key={u.id}>{u.email}</li>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
